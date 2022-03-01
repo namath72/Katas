d0965e4d36efe797abc413e025041418a8ce4d62
Fernando Fawcett <Fernando.Fawcett@Marshall.co.uk>
2022-03-01 09:27:33 +0000

    Added test project

---
+++ Katas.Test/KatasTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+public class KatasTest
+{
+    [Test]
+    public void IpsBetweenTest()
+    {
+        Assert.AreEqual(50, Katas.Katas.IpsBetween("10.0.0.0", "10.0.0.50"));
+        Assert.AreEqual(256, Katas.Katas.IpsBetween("10.0.0.0", "10.0.1.0"));
+        Assert.AreEqual(246, Katas.Katas.IpsBetween("20.0.0.10", "20.0.1.0"));
+    }
+
+    [Test]
+    [TestCase(155, ExpectedResult = -1)]
+    [TestCase(121, ExpectedResult = 144)]
+    [TestCase(625, ExpectedResult = 676)]
+    [TestCase(319225, ExpectedResult = 320356)]
+    [TestCase(15241383936, ExpectedResult = 15241630849)]
+    public static long FindNextSquare(long num)
+    {
+        return Katas.Katas.FindNextSquare(num);
+    }
+
+    [Test]
+    public void ReverseWordsTest()
+    {
+        Assert.AreEqual("sihT si na !elpmaxe", Katas.Katas.ReverseWords("This is an example!"));
+    }
+
+}
---
+++ Katas.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Katas.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Katas.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("401f2132-3e61-47b9-8a8d-e53658852221")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
